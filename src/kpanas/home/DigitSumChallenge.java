f689ecfd9412f83102fd8e430d7599c4342b57ff
chris <panas_k@o2.pl>
2021-02-28 17:09:43 +0100

    solution of Digit Sum Challenge

--- src/kpanas/home/DigitSumChallenge.java
+++ src/kpanas/home/DigitSumChallenge.java
@@ -1,9 +1,13 @@
 package kpanas.home;
+// write a method which sum all the digits in given number, grather than 9
 
 public class DigitSumChallenge {
 
     public static void main(String[] args) {
         System.out.println(sumDigits(125));
+        System.out.println(sumDigits(-125));
+        System.out.println(sumDigits(4));
+        System.out.println(sumDigits(32123));
 
     }
 
